ecb45ab04de7393d2d4669b57d76eb9fcd611e62
Adam DAVID <adam.david@polytechnique.edu>
2021-04-26 17:15:57 +0200

    commit

--- src/DiscreteHawkesProcess.jl
+++ src/DiscreteHawkesProcess.jl
@@ -1,4 +1,52 @@
 
+"""
+DiscreteSEPPExpKern(μ, ϕ, γ)
+
+*DiscreteSEPPExpKern* is a discrete SEPP with exponential kernel model type with rate λ (for all t, there is an event with probability λ(t)) viz. :
+
+λ(t) = μ + ϕ \sum_{t_k < t} \exp(γ(t-t_k))
+
+where t_k are the timestamps of all events.
+"""
+
+struct DiscreteSEPPExpKern
+    μ::Real
+    ϕ::Real
+    γ::Real
+    DiscreteSEPPExpKern(μ, ϕ, γ) = any((μ, ϕ, γ) .< 0) ? error("paramaters must be positive or zero") : new(μ, ϕ, γ)
+end
+
+"""
+DiscreteSEMPPExpKern(μ, ϕ, γ, markdens, α, β)
+
+*DiscreteSEMPPExpKern* is a discrete SEMPP with exponential kernel model type such that for all t,m , there is an event in t with mark m with probability λ_g(t)f(m|t)) where :
+
+λ_g(t) = μ + ϕ * ν(t)
+
+with ν(t) = \sum_{t_k < t} (1 + δ*m_k) (\exp(γ(t-t_k))
+
+f(m|t) is markdens with scale σ_t = β + α * ν(t)
+
+t_k are the timestamps of all events and m_k their marks.
+"""
+
+struct DiscreteSEMPPExpKern
+    μ::Real
+    ϕ::Real
+    γ::Real
+    markdens::ContinuousUnivariateDistribution
+    α::Real
+    β::Real
+    DiscreteSEPPExpKern(μ, ϕ, γ, markdens, α, β) = any((μ, ϕ, γ, α, β) .< 0) ? error("paramaters must be positive or zero") : new(μ, ϕ, γ, markdens, α, β)
+end
+
+
+"""
+volfunc(when, pp, γ, δ)
+
+*volfunc* is the term in the rate of a SEPP that corresponds to the self-excitement. In Li2020 it is denoted by ν.
+"""
+
 function volfunc(when::AbstractVector, pp::PP, γ::Real, δ::Real = 0)
     
     γ >= 0 && error("γ must be positive or zero")
@@ -25,8 +73,16 @@ function volfunc(when::AbstractVector, pp::PP, γ::Real, δ::Real = 0)
     return self_ex.(when)
 end
 
+"""
+negloglik(pp, markdens, μ, ϕ, γ, δ, ξ, β, α)
 
-function negloglik(pp::PointProcess, markdens::Function ;  μ::Real, ϕ::Real, γ::Real, δ::Real = 0, ξ::Real, β::Real, α::Real, κ::Real = 1)
+*negloglik* calculates the negative log-likelihood of the SE(M)PP with the events and paramaters passed in arguments. 
+
+TODO : if negloglik is to be exported, it might be useful to add methods so that negloglik can take a model as an argument instead of its paramaters
+"""
+
+
+function negloglik(pp::PointProcess, markdens::ContinuousUnivariateDistribution ;  μ::Real, ϕ::Real, γ::Real, δ::Real = 0, ξ::Real, β::Real, α::Real, κ::Real = 1)
     
     (all((μ, ϕ, γ, δ, β, α, κ) .>= 0)) && ((μ, ϕ, γ, δ, β, α, κ) = abs.((μ, ϕ, γ, δ, β, α, κ)) ; warn("all paramaters except for ξ must be positive or zero, taking absolute value"))
 
