2aad444ceed13b1803e0b81acafdbcffc40cca63
Adam DAVID <adam.david@polytechnique.edu>
2021-04-29 14:23:39 +0200

    commit

--- test/DiscreteHawkesProcess_tests.jl
+++ test/DiscreteHawkesProcess_tests.jl
@@ -1,16 +1,24 @@
 @testset "DiscreteHawkesProcess.jl" begin
+
     when = [1,2,3]
     times = [1,3]
-    marks = [42.0, 4.2]
+    marks = [2, 3]
+
     pp = SEMPP.PointProcess(times)
     mpp = SEMPP.MarkedPointProcess(times, marks)
-    μ = 3
-    ϕ = 2.1
-    γ = 7
+
+    μ = 0
+    ϕ = 1
+    γ = 1
     δ = 1.0
-    @test_throws ErrorException SEMPP.volfunc(when, pp, -γ, δ)
-    @test_logs (:warn,"no marks but δ non zero") SEMPP.volfunc(when, pp, γ, δ)
-    @test size(SEMPP.volfunc(when, mpp, γ, δ)) == size(when)
+
+    @testset "volfunc" begin
+        @test_throws ErrorException SEMPP.volfunc(when, pp, -γ, δ)
+        @test_logs (:warn,"no marks but δ non zero") SEMPP.volfunc(when, pp, γ, δ)
+        @test size(SEMPP.volfunc(when, mpp, γ, δ)) == size(when)
+        @test SEMPP.volfunc(when, mpp, γ, δ) == [0, 3*exp(-1), 3*exp(-2)]
+    end
+
     GPD = Distributions.GeneralizedPareto
     EGPD1 = EGPD.EGPpower
     ξ = 1.5
--- test/PP_tests.jl
+++ test/PP_tests.jl
@@ -1,5 +1,3 @@
-using SEMPP
-
 @testset "PP.jl" begin
     t1 = 1
     t2 = 2
@@ -7,6 +5,8 @@ using SEMPP
     marks = [4.2, -7]
     pp = SEMPP.PointProcess(times)
     mpp = SEMPP.MarkedPointProcess(times, marks)
+    times2 = [1, 2, 3]
+    @test_throws ErrorException SEMPP.MarkedPointProcess(times2, marks)
     @test SEMPP.ground_process(mpp).times == pp.times
     @test SEMPP.start_time(pp) == t1
     @test SEMPP.start_time(mpp) == t1
