9d2188057023cccec6eda7a540f8363c5032ab1b
Adam DAVID <adam.david@polytechnique.edu>
2021-05-17 18:15:21 +0200

    plots fix

--- src/Plots/ModelPlots.jl
+++ src/Plots/ModelPlots.jl
@@ -21,7 +21,9 @@ function rate_plot(sepp::SEPP; step = nothing, from_idx = nothing, to_idx = noth
     
     rate_layer = layer(x = anytimes, y = lamb, color = [color("black")], Geom.line)
 
-    plt = plot(x = times[from_idx:to_idx], y = points, size = [0mm], Geom.point, Guide.xrug, Theme(grid_line_width=0mm), rate_layer)
+    points_layer = layer(x = times[from_idx:to_idx], y = points, alpha = [0.5], Geom.point)
+
+    plt = plot(rate_layer, points_layer, Theme(grid_line_width=0mm))
 
     return plt
 end
@@ -31,11 +33,7 @@ function marks_plot(mts::MarkedTimeSeries)
     marks = mts.marks
     times = mts.times
 
-    plt = plot(x=times, y=marks, Geom.point) 
-    
-    for k in 1:length(times)
-        push!(plt, layer(x=[times[k],times[k]], y=[0, marks[k]], Geom.line))
-    end
+    plt = plot(x=times, y=marks, Geom.hair, Geom.point) 
 
     return plt
 end
