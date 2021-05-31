657faad95cd6f5009ff3f9e98e54c613fb475322
Adam DAVID <adam.david@polytechnique.edu>
2021-05-31 11:33:13 +0200

    typo fix

--- test/ParametersEstimation_tests.jl
+++ test/ParametersEstimation_tests.jl
@@ -3,8 +3,8 @@
     times = [1,3]
     marks = [2, 3]
 
-    pp = SEMPP.PointProcess(times)
-    mpp = SEMPP.MarkedPointProcess(times, marks)
+    ts = TimeSeries(times)
+    mts = MarkedTimeSeries(times, marks)
 
     μ = 2.0
     ϕ = 1
@@ -12,10 +12,10 @@
     δ = 1.0
 
     @testset "volfunc" begin
-        @test_throws ErrorException SEMPP.volfunc(when, pp, -γ, δ)
-        @test_logs (:warn,"no marks but δ non zero") SEMPP.volfunc(when, pp, γ, δ)
-        @test size(SEMPP.volfunc(when, mpp, γ, δ)) == size(when)
-        @test SEMPP.volfunc(when, mpp, γ, δ) == [0, 3*exp(-1), 3*exp(-2)]
+        @test_throws ErrorException SEMPP.volfunc(when, ts, -γ, δ)
+        @test_logs (:warn,"no marks but δ non zero") SEMPP.volfunc(when, ts, γ, δ)
+        @test size(SEMPP.volfunc(when, mts, γ, δ)) == size(when)
+        @test SEMPP.volfunc(when, mts, γ, δ) == [0, 3*exp(-1), 3*exp(-2)]
     end
 end
 
