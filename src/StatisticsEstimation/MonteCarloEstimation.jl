672902cde2fd171d155ea8f716a9388d418a7c27
Adam DAVID <adam.david@polytechnique.edu>
2021-07-05 12:40:14 +0200

    rQy estimator

--- src/StatisticsEstimation/MonteCarloEstimation.jl
+++ src/StatisticsEstimation/MonteCarloEstimation.jl
@@ -93,3 +93,60 @@ function monte_carlo_return_period(sepp::SEMPPExpKern; magnitude::Real, r::Integ
 
     return ret_per
 end
+
+
+function rQy(sempp::SEMPPExpKern, r::Integer = 7, y::Real = 2; horiz::Union{Real, Nothing} = nothing, M::Integer = 500)
+    sims = MarkedTimeSeries[]
+    horiz = isnothing(horiz) ? 5*y*365 : Int(horiz*365)
+
+    mag_min = 0
+    mag_max = 0
+
+    for i in 1:M
+        sim = discrete_simulation(sempp; start_time = 0, end_time = horiz)
+        mag_min = min(mag_min, minimum(sim.marks))
+        mag_max = max(mag_max, maximum(sim.marks))
+        push!(sims, sim)
+    end
+
+    function count(mts::MarkedTimeSeries, magnitude::Real)::Real
+        times = mts.times
+        marks = mts.marks
+        
+        res = 0
+        c = 0
+        marks[1] >= magnitude && (c = 1)
+
+        for i in 2:length(times)
+            if (marks[1] >= magnitude) && (c == 0 || times[i-1] == times[i] - 1)
+                c+=1
+            else
+                c >= r && (res += 1)
+                c=0
+            end      
+        end
+
+        c >= r && (res += 1)
+
+        res == 0 && (res = 1 ; warn += 1)
+
+        return horiz/res
+    end
+
+    ret_per = 0
+    ret_per_mem = 1
+    
+    while ret_per != ret_per_mem
+        ret_per_mem = ret_per
+        mag = 0.5 * (mag_max + mag_min)
+        ret_per = median((x -> count(x, mag)).(sims))
+
+        if ret_per > r
+            mag_min = mag
+        else
+            mag_max = mag    
+        end
+    end
+
+    return mag_min, mag_max, ret_per
+end
